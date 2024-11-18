335fd204f050f890e90987fc0924481f29988573
Shiven <ziyawaais@gmail.com>
2024-11-18 23:43:22 +0530

    Delete barakties original directory

--- barakties original/style.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll('.team-member').forEach(member => {
-    member.addEventListener('click', () => {
-      // Remove 'clicked' class from all members
-      document.querySelectorAll('.team-member').forEach(item => item.classList.remove('clicked'));
-      // Add 'clicked' class to the clicked member
-      member.classList.add('clicked');
-    });
-  });
-  
